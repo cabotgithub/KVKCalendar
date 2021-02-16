7d6f8328334f5565c3d7e0d9f7299b319c250aba
cabotgithub <arun.joseph@cabotsolutions.com>
2021-02-16 11:33:28 +0530

    dot view alignment fix

--- Sources/KVKCalendar/ListViewCell.swift
+++ Sources/KVKCalendar/ListViewCell.swift
@@ -47,26 +47,22 @@ final class ListViewCell: UITableViewCell {
         return spaceView
     }()
     
+    private var containerView: UIView = {
+        let container = UIView()
+        return container
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(txtLabel)
-        contentView.addSubview(dotView)
         contentView.addSubview(spaceView)
+        contentView.addSubview(containerView)
+        containerView.addSubview(txtLabel)
+        containerView.addSubview(dotView)
         spaceView.backgroundColor = cellSpaceColor
         dotView.translatesAutoresizingMaskIntoConstraints = false
         txtLabel.translatesAutoresizingMaskIntoConstraints = false
         spaceView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let leftDot: NSLayoutConstraint
-        if #available(iOS 11.0, *) {
-            leftDot = dotView.leftAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leftAnchor)
-        } else {
-            leftDot = dotView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 15)
-        }
-        let centerYDot = dotView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-        let widthDot = dotView.widthAnchor.constraint(equalToConstant: 20)
-        let heightDot = dotView.heightAnchor.constraint(equalToConstant: 20)
-        NSLayoutConstraint.activate([leftDot, centerYDot, widthDot, heightDot])
+        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         let spaceViewLeading = spaceView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 0)
         let spaceViewTrailing = spaceView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: 0)
@@ -74,11 +70,29 @@ final class ListViewCell: UITableViewCell {
         let spaceViewHeight = spaceView.heightAnchor.constraint(equalToConstant: 12)
         NSLayoutConstraint.activate([spaceViewBottom, spaceViewLeading, spaceViewTrailing, spaceViewHeight])
         
-        let topTxt = txtLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15)
-        let bottomTxt = txtLabel.bottomAnchor.constraint(equalTo: spaceView.topAnchor, constant: -15)
+        let containerTopAnchor = containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0)
+        let continerBottomAnchor = containerView.bottomAnchor.constraint(equalTo: spaceView.topAnchor, constant: 0)
+        let containerLeftAnchor = containerView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 0)
+        let containerRightAnchor = containerView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: 0)
+        NSLayoutConstraint.activate([containerTopAnchor, continerBottomAnchor, containerLeftAnchor, containerRightAnchor])
+        
+        let leftDot: NSLayoutConstraint
+        if #available(iOS 11.0, *) {
+            leftDot = dotView.leftAnchor.constraint(equalTo: containerView.layoutMarginsGuide.leftAnchor)
+        } else {
+            leftDot = dotView.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 15)
+        }
+        let centerYDot = dotView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+        let widthDot = dotView.widthAnchor.constraint(equalToConstant: 20)
+        let heightDot = dotView.heightAnchor.constraint(equalToConstant: 20)
+        NSLayoutConstraint.activate([leftDot, centerYDot, widthDot, heightDot])
+        
+        let topTxt = txtLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 15)
+        let bottomTxt = txtLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -15)
         let leftTxt = txtLabel.leftAnchor.constraint(equalTo: dotView.rightAnchor, constant: 10)
-        let rightTxt = txtLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -15)
+        let rightTxt = txtLabel.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -15)
         NSLayoutConstraint.activate([topTxt, bottomTxt, leftTxt, rightTxt])
+        
     }
     
     required init?(coder: NSCoder) {
